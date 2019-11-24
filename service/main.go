3d70ea8ef93e5026804a4f11e3c431660fa55275
StrandingHeart <m17600031770@163.com>
2019-11-24 21:39:39 +0800

    init

---
+++ service/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/oauth2.v3/errors"
+	"gopkg.in/oauth2.v3/manage"
+	"gopkg.in/oauth2.v3/models"
+	"gopkg.in/oauth2.v3/server"
+	"gopkg.in/oauth2.v3/store"
+	"log"
+	"net/http"
+)
+
+func main() {
+	manager := manage.NewDefaultManager()
+	// token memory store
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+
+	// client memory store
+	clientStore := store.NewClientStore()
+	err := clientStore.Set("zhangyong_client_id", &models.Client{
+		ID:     "1",
+		Secret: "zhangyong_client_secret",
+		Domain: "http://localhost",
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	manager.MapClientStorage(clientStore)
+
+	srv := server.NewDefaultServer(manager)
+	srv.SetAllowGetAccessRequest(true)
+	srv.SetClientInfoHandler(server.ClientFormHandler)
+
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
+
+	http.HandleFunc("/authorize", func(w http.ResponseWriter, r *http.Request) {
+		err := srv.HandleAuthorizeRequest(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	})
+//http://localhost:9096/token?grant_type=client_credentials&client_id=zhangyong_client_id&client_secret=zhangyong_client_secret&scope=read
+//获取 access_token
+	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+		err := srv.HandleTokenRequest(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":9096", nil))
+}
